26692192ed34a9d91a38b9844cf8369df0a019b4
Matt Watts <matthew.watts.mw@gmail.com>
2025-02-20 15:04:49 -0600

    5_uart_tx

--- 4_gpio_input/Src/main.c
+++ 4_gpio_input/Src/main.c
@@ -2,28 +2,31 @@
 
 #define GPIOAEN				(1U << 0)
 #define PIN_5				(1U << 5)
+#define PIN_13				(1U << 13)
+#define BTN_PIN				(PIN_13)
 #define LED_PIN				(PIN_5)
 
 int main(void) {
 	// enable clock for AHB1 BUS
 	RCC->AHB1ENR |= GPIOAEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
-	// set up pin for output mode
+	// set up pin 5 for output mode
 	GPIOA->MODER |= (1U << 10);
 	GPIOA->MODER &= ~(1U << 11);
 
+	// set up pin 13 for input mode
+	GPIOC->MODER &= ~(1U << 26);
+	GPIOC->MODER &= ~(1U << 27);
+
 	while (1) {
-		// turn LED on
-		GPIOA->BSRR = LED_PIN;
-
-		for (volatile int i = 0; i < 100000; i++)
-			;
-
-		// turn LED off
-		GPIOA->BSRR = LED_PIN << 16;
-
-		for (volatile int i = 0; i < 100000; i++)
-			;
+		// if button is pressed pin 13 will be high
+		if (GPIOC->IDR & BTN_PIN) {
+			// turn LED on
+			GPIOA->BSRR = LED_PIN;
+		} else {
+			// turn LED off
+			GPIOA->BSRR = LED_PIN << 16;
+		}
 	}
 }
-
